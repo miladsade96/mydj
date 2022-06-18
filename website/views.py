59112b5a7d0ec6db2d6335f689a774e1b4f22a8b
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-18 18:38:55 +0430

    Defining views

--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+
+def http_test(request):
+    return HttpResponse("<h1>This is a test</h1?")
+
+
+def json_test(request):
+    return JsonResponse({"Name": "ELNS"})
