0dd96bcc6fdf3bd260d18cb53040f04ba36bc9d5
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-19 10:36:27 +0430

    Adding general static files address to settings.py

--- mydj/settings.py
+++ mydj/settings.py
@@ -124,6 +124,10 @@ STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
