b275e1fd2cf02d0b6126027bf3a179178f773afc
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-20 12:37:07 +0430

    Fixing blog app urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from blog.views import *
 app_name = "blog"
 
 urlpatterns = [
-    path("blog", blog_view, name="index"),
-    path("blog/single", blog_single, name="single")
+    path("", blog_view, name="index"),
+    path("single", blog_single, name="single")
 ]
