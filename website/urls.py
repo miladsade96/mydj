1e89e3a0d911095839871c0c8152c4a63a473fb4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-20 13:58:11 +0430

    Adding test url

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from website.views import index_view, about_view, contact_view
+from website.views import index_view, about_view, contact_view, test_view
 
 # Defining app name
 app_name = "website"
@@ -7,5 +7,6 @@ app_name = "website"
 urlpatterns = [
     path("", index_view, name="index"),
     path("about", about_view, name="about"),
-    path("contact", contact_view, name="contact")
+    path("contact", contact_view, name="contact"),
+    path("test", test_view, name="test")
 ]
