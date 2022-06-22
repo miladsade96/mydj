6218258d0c2bbe720ed50600c80ceca2a34bbddd
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-22 21:38:05 +0430

    Defining Post model class

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    counted_views = models.PositiveIntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
