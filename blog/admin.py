c04163e255b12d3b52c312dd4f9d5d3b67471ec3
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-23 12:12:06 +0430

    Defining and customizing PostAdmin class

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from blog.models import Post
 
+
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = '-empty-'
+    list_display = ('title', 'counted_views', 'status', 'created_date', 'published_date')
+    list_filter = ('status', 'created_date', 'published_date')
+    ordering = ('-created_date',)
+    search_fields = ('title', 'content')
