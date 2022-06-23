16dea0c86a80b86a0ee72ad8d6a534d6fe4febc9
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-23 13:00:04 +0430

    Defining and customizing ContactAdmin class

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from website.models import Contact
+
 
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_date')
+    list_filter = ('email',)
+    search_fields = ('name', 'message', 'subject')
+    date_hierarchy = 'created_date'
