f0723109314c14e4145ef83f838d375115e13a57
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-23 11:46:00 +0430

    Overwriting str method for Post class

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,6 @@ class Post(models.Model):
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.id}"
