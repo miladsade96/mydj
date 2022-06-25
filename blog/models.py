12b6dc0c277ba0cf11c70938b50dafb8e7fac9a1
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-25 17:21:14 +0430

    Adding author to Post model

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     counted_views = models.PositiveIntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
