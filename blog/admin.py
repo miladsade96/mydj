e291878b95363e19ccb73fae1c88b71e825f2a82
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-25 17:35:49 +0430

    Customizing admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -7,6 +7,6 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
     empty_value_display = '-empty-'
-    list_display = ('title', 'counted_views', 'status', 'created_date', 'published_date')
-    list_filter = ('status', 'created_date', 'published_date')
+    list_display = ('title', 'author', 'counted_views', 'status', 'created_date', 'published_date')
+    list_filter = ('author', 'status', 'created_date', 'published_date')
     search_fields = ('title', 'content')
