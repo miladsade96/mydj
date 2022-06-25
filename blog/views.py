19d817f25771170d986148f37a6d3740c351a8ea
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-25 13:59:30 +0430

    Extending blog view functions

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
 
 # Create your views here.
 def blog_view(request):
-    return render(request, "blog/blog-home.html")
+    posts = Post.objects.filter(status=1)
+    context = {"posts": posts}
+    return render(request, "blog/blog-home.html", context)
 
 
-def blog_single(request):
-    return render(request, "blog/blog-single.html")
+def blog_single(request, pid):
+    post = get_object_or_404(Post, id=pid)
+    context = {"post": post}
+    return render(request, "blog/blog-single.html", context)
