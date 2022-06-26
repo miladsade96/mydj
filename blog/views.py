b7782f734445ea4ef604935ec24e25100b19e866
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-26 10:11:44 +0430

    Implementing homework

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.utils import timezone
 
 
 # Create your views here.
 def blog_view(request):
-    posts = Post.objects.filter(status=1)
+    posts = Post.objects.filter(status=1, published_date__lte=timezone.now()).order_by('-published_date')
     context = {"posts": posts}
     return render(request, "blog/blog-home.html", context)
 
 
 def blog_single(request, pid):
-    post = get_object_or_404(Post, id=pid, status=1)
+    post = get_object_or_404(Post, id=pid, status=1, published_date__lte=timezone.now())
     increment_views(pid)
     context = {"post": post}
     return render(request, "blog/blog-single.html", context)
