5cd11efcf3988033956c506d76ab5f366557acf5
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-28 12:07:42 +0430

    Defining n_published_posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,9 @@
 from django import template
+from blog.models import Post
 
 register = template.Library()
+
+
+@register.simple_tag(name="n_published_posts")
+def get_number_of_published_posts():
+    return Post.objects.filter(status=1).count()
