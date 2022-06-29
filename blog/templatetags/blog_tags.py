f27274ff698a9cf65e23698995f58896356a7623
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-29 11:01:47 +0430

    Defining post_categories template tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -23,3 +23,13 @@ def snippet(value, word_count):
 def latest_posts(count=3):
     posts = Post.objects.filter(status=1).order_by("-published_date")[:count]
     return {"posts": posts}
+
+
+@register.inclusion_tag("blog/blog-post-categories.html")
+def post_categories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = dict()
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {"categories": cat_dict}
