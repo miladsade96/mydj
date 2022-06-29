36ba300f826955e4578e0c1832ff97a356062011
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-29 12:19:07 +0430

    Adding blog category url

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ app_name = "blog"
 
 urlpatterns = [
     path("", blog_view, name="index"),
-    path("<int:pid>", blog_single, name="single")
+    path("<int:pid>", blog_single, name="single"),
+    path("category/<str:cat_name>", blog_view, name="category")
 ]
