804b3b4f24727b055f51f0249d8b92fcb4e0c114
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-03 23:22:17 +0430

    Defining Newsletter model

--- website/models.py
+++ website/models.py
@@ -15,3 +15,10 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Newsletter(models.Model):
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
