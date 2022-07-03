53d9f5e857ef1d0d6beb8bd032d2d999e6d72859
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-03 23:53:26 +0430

    Defining newsletter url

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from website.views import index_view, about_view, contact_view
+from website.views import index_view, about_view, contact_view, newsletter_view
 
 # Defining app name
 app_name = "website"
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", index_view, name="index"),
     path("about", about_view, name="about"),
     path("contact", contact_view, name="contact"),
+    path("newsletter", newsletter_view, name="newsletter"),
 ]
