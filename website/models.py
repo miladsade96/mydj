6846cb6b397edd6da8f9c7c0b0daec62f888332d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-04 12:50:05 +0430

    Allowing subject to be null and blank

--- website/models.py
+++ website/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
-    subject = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255, null=True, blank=True)
     message = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
