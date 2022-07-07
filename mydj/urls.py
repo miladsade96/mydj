dace2b47e860e800a6086f23f557d2fd5122eaea
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-07 21:14:26 +0430

    Adding django debug toolbar url

--- mydj/urls.py
+++ mydj/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from website.sitemaps import StaticViewSitemap
 from blog.sitemaps import BlogSitemap
+import debug_toolbar
 
 
 sitemaps = {
@@ -32,7 +33,8 @@ urlpatterns = [
     path("", include("website.urls")),
     path("blog/", include("blog.urls")),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
-    path("robots.txt", include("robots.urls"))
+    path("robots.txt", include("robots.urls")),
+    path("__debug__/", include(debug_toolbar.urls)),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
