854183cfdef2da08b1a517bd5aa9d7cc6b9ced58
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-07 18:32:16 +0430

    Defining get_absolute_url method to Post model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -29,3 +30,6 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+    def get_absolute_url(self):
+        return reverse('blog:single', kwargs={'pid': self.id})
