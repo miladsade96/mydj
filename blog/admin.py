a74203694b1386dc1968ebd9606cf06b994a7577
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-08 15:06:46 +0430

    Adding django_summernote functionality to Post model content field

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from blog.models import Post, Category
+from django_summernote.admin import SummernoteModelAdmin
 
 
 # Register your models here.
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     date_hierarchy = "created_date"
     empty_value_display = '-empty-'
     list_display = ('title', 'author', 'counted_views', 'status', 'created_date', 'published_date')
     list_filter = ('author', 'status', 'created_date', 'published_date')
     search_fields = ('title', 'content')
+    summernote_fields = ('content',)
 
 
 @admin.register(Category)
