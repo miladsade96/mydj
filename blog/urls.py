08858136df78e1de90cbfad5be7da99ed76d3bd0
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-11 19:03:45 +0430

    Adding rss feed url to blog urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from blog.views import *
+from blog.feeds import LatestPostsRSSFeed
 
 app_name = "blog"
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path("tag/<str:tag_name>", blog_view, name="tag"),
     path("author/<str:author_username>", blog_view, name="author"),
     path("search/", blog_search, name="search"),
+    path("rss/feed", LatestPostsRSSFeed(), name="rss_feed"),
 ]
