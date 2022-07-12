b6c0a66cabd4d4fdc4429ab7e38e4f74990869bf
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-12 18:47:43 +0430

    Defining accounts view functions - part1

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+
 
 # Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            pass
+    return render(request, 'accounts/login.html')
+
+
+def logout_view(request):
+    pass
+
+
+def signup_view(request):
+    return render(request, 'accounts/signup.html')
