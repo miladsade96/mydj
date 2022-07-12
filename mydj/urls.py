519207394f82eede6a719491526054e26e6daeb4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-12 13:46:36 +0430

    Adding accounts app url to project urls.py

--- mydj/urls.py
+++ mydj/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("website.urls")),
     path("blog/", include("blog.urls")),
+    path("accounts/", include("accounts.urls")),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
     path("robots.txt", include("robots.urls")),
     path("__debug__/", include(debug_toolbar.urls)),
