168d8742a182f2896fd5839509db5effb9175a73
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-12 13:43:54 +0430

    Adding accounts app to settings.py

--- mydj/settings.py
+++ mydj/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "website",
     "blog",
+    "accounts",
     "django.contrib.humanize",
     "django_extensions",
     "django.contrib.sites",
