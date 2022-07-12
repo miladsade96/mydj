c07fc2d04e39231e338a78126c1de15750fa5748
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-12 13:52:23 +0430

    Defining account app urls

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from accounts.views import login_view, logout_view, signup_view
+
+# Defining app name
+app_name = "accounts"
+
+urlpatterns = [
+    path("login", login_view, name="login"),
+    path("logout", logout_view, name="logout"),
+    path("signup", signup_view, name="signup"),
+]
