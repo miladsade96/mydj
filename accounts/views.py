db3b9e6565bd5c981bef52d6faa8eb393a11f6ae
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-13 11:16:07 +0430

    Using django authentication form instead

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 
 # Create your views here.
 def login_view(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        else:
-            pass
-    return render(request, 'accounts/login.html')
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('/')
+                else:
+                    pass
+        form = AuthenticationForm()
+        context = {'form': form}
+        return render(request, 'accounts/login.html', context)
+    else:
+        return redirect('/')
+
 
 
 def logout_view(request):
