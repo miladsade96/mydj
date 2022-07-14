92a6e5e142c73bd84481ca0e91ce877b6bcbc877
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-14 08:54:41 +0430

    Adding login_require field to Post model

--- blog/models.py
+++ blog/models.py
@@ -21,6 +21,7 @@ class Post(models.Model):
     tags = TaggableManager()
     counted_views = models.PositiveIntegerField(default=0)
     status = models.BooleanField(default=False)
+    login_require = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
