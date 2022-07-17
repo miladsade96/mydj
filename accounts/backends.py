73b99ed990239d9980d77811c54bb0d6ac7e4143
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-17 14:02:34 +0430

    Fixing EmailBackend

--- accounts/backends.py
+++ accounts/backends.py
@@ -3,19 +3,20 @@ from django.contrib.auth import get_user_model
 from django.db.models import Q
 
 
-UserModel = get_user_model()
-
-
 class EmailBackend(ModelBackend):
 
     def authenticate(self, request, username=None, password=None, **kwargs):
+        user_model = get_user_model()
         try:
-            user = UserModel.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
-        except UserModel.DoesNotExist:
-            UserModel.set_password(password)
-            return
-        except UserModel.MultipleObjectsReturned:
-            user = UserModel.objects.get(Q(username__iexact=username) |
-                                         Q(email__iexact=username)).order_by('id').first()
-        if user.check_password(password) and self.user_can_authenticate(user):
-            return user
+            user = user_model.objects.get(Q(email=username) | Q(username=username))
+            if user.check_password(password):
+                return user
+        except user_model.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        user_model = get_user_model()
+        try:
+            return user_model.objects.get(pk=user_id)
+        except user_model.DoesNotExist:
+            return None
