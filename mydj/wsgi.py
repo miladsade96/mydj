4256d957179ee506e8432c6cfa543b8cab4f0c2e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-07-20 13:10:43 +0430

    Changing wsgi setting to prod

--- mydj/wsgi.py
+++ mydj/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydj.setting.prod')
 
 application = get_wsgi_application()
